392ec78020b492b2547ea36bf90cd994ebf1d920
Scali <none@none>
2018-02-01 21:05:24 +0100

    Remove near/far/huge pointer definitions from code with dummy #defines.

--- C/32bit.h
+++ C/32bit.h
@@ -1,3 +1,6 @@
+#ifndef _32BIT_H_
+#define _32BIT_H_
+
 #if defined(M_I386)
 #define farmalloc malloc
 #define farfree free
@@ -7,4 +10,10 @@
 #define halloc malloc
 #define hfree free
 #define _farfread fread
+
+#define near
+#define far
+#define huge
 #endif
+
+#endif /* _32BIT_H_ */
