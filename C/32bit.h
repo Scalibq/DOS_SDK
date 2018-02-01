6fba39be633a4f7fc2252872ec65c7cdb96c2789
Scali <none@none>
2018-02-01 16:52:11 +0100

    Add the proper conditional compile flags, so the aliases are only effective on 32-bit code, as intended.

--- C/32bit.h
+++ C/32bit.h
@@ -1,4 +1,4 @@
-/*
+#if defined(M_I386)
 #define farmalloc malloc
 #define farfree free
 #define _fmalloc malloc
@@ -6,4 +6,5 @@
 #define _frealloc realloc
 #define halloc malloc
 #define hfree free
-*/
+#define _farfread fread
+#endif
